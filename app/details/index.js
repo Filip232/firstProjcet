ea0af17977dbb0d8e19c859a290d60486550c94c
filip232 <filip.wesolowski@onet.eu>
2020-09-03 12:52:11 +0200

    new stuff

--- app/details/index.js
+++ app/details/index.js
@@ -3,3 +3,22 @@ import '../styles/main.scss'
 if (process.env.NODE_ENV !== 'production') {
   require('./details.pug')
 }
+
+const getNote = id => {
+  return fetch('http://localhost:4000/details/' + id)
+    .then(res => res.json())
+    .then(data => {
+      let div = document.createElement('div')
+      div.id = data.id + 'div'
+      document.body.appendChild(div)
+      let box = document.getElementById(data.id + 'div')
+      let p = document.createElement('p')
+      p.textContent = data.text
+      box.appendChild(p)
+    })
+}
+
+let dupa = window.location.href
+let short = dupa.substring(dupa.indexOf('=') + 1, dupa.length)
+
+getNote(short)
--- app/index.js
+++ app/index.js
@@ -4,6 +4,8 @@ if (process.env.NODE_ENV !== 'production') {
   require('./index.pug')
 }
 
+let multiNotes = []
+
 const getNotes = () => {
   return fetch('http://localhost:4000/notes')
     .then(res => res.json())
@@ -20,9 +22,24 @@ const getNotes = () => {
         trash.setAttribute('data-Id', element.id)
         box.appendChild(p)
         box.appendChild(trash)
+        let anchor = document.createElement('a')
+        anchor.textContent = 'Details'
+        anchor.setAttribute('href', '/details?id=' + element.id)
+        box.appendChild(anchor)
+        let checkboxbutton = document.createElement('button')
+        checkboxbutton.setAttribute('class', 'checkbox')
+        box.appendChild(checkboxbutton)
         trash.addEventListener('click', () => {
           removeNote(trash.getAttribute('data-Id'))
           location.reload()
+        })
+        checkboxbutton.addEventListener('click', () => {
+          if (multiNotes.indexOf(element.id) === -1) {
+            multiNotes.push(element.id)
+          } else {
+            multiNotes.splice(multiNotes.indexOf(element.id), 1)
+          }
+          console.log(multiNotes)
         })
       })
     })
@@ -63,3 +80,11 @@ const removeNote = id => {
   return fetch('http://localhost:4000/notes/' + id, options)
     .catch(() => console.log('ERROR'))
 }
+
+const deleteAll = document.getElementById('deleteAll')
+deleteAll.addEventListener('click', () => {
+  for (let i = 0; i < multiNotes.length; i++) {
+    removeNote(multiNotes[i])
+  }
+  location.reload()
+})
